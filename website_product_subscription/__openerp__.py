83f9d9fbb57dd99b8376353943bb4dbb5691801a
houssine <houssine.bakkali@gmail.com>
2019-08-23 15:00:45 +0200

    [IMP] remove commented dependency

--- website_product_subscription/__openerp__.py
+++ website_product_subscription/__openerp__.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019 Coop IT Easy SCRL fs
-#   Houssine Bakkali <Houssine@coopiteasy.be>
+#   Houssine Bakkali <houssine@coopiteasy.be>
 #   Robin Keunen <robin@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Website Product Subscription",
-    "version": "1.0",
+    "version": "9.0.1.0.1",
     "depends": [
         "base",
         "website",
         "portal",
         "website_recaptcha_reloaded",
         "product_subscription",
-        # "base_vat",
     ],
     "author": "Coop IT Easy SCRL",
     "category": "Sales",
     "description": """
-    This module add the product subscription forms on the website. It will allow
-    to subscribe online for a subscription template.
+    This module add the product subscription forms on the website. It will
+     allow to subscribe online for a subscription template.
     """,
     'data': [
         'views/product_subscription_template.xml',
