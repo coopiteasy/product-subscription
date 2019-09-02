fcd5a2b20ab2b75671bece730e5903b742f8cb77
houssine <houssine.bakkali@gmail.com>
2019-09-02 19:06:40 +0200

    [IMP] put template ids in local static variable
    [FIX] resolve conflict
    
    [FIX] resolve conflict
    
    [IMP] remove commented code

--- website_product_subscription_mollie_payment/__init__.py
+++ website_product_subscription_mollie_payment/__init__.py
@@ -1,9 +1,2 @@
-<<<<<<< Upstream, based on branch '9.0_enable_online_payment' of https://github.com/coopiteasy/product-subscription.git
-<<<<<<< Upstream, based on branch '9.0_enable_online_payment' of https://github.com/coopiteasy/product-subscription.git
 from . import controllers
-=======
->>>>>>> f2f6829 [IMP] make it generic
-=======
-from . import controllers
->>>>>>> 576a247 [IMP] dedicated thanks page
 from . import models
