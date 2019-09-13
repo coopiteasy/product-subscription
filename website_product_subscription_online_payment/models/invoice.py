a26e0cdefd57afe62d404c538f61e72c2729e6b6
houssine <houssine.bakkali@gmail.com>
2019-09-13 15:17:28 +0200

    [IMP] payment confirmation mail shouldn't be sent when split payment

--- website_product_subscription_online_payment/models/invoice.py
+++ website_product_subscription_online_payment/models/invoice.py
@@ -12,3 +12,15 @@ class AccountInvoice(models.Model):
             self.process_subscription(effective_date)
 
         return True
+
+    @api.multi
+    def send_confirm_paid_email(self):
+        """Send an email to confirm the payment of this invoice."""
+        conf_email_template = self.env.ref(
+            'product_subscription'
+            '.subscription_payment_confirmation_email_template'
+        )
+        for invoice in self:
+            request = invoice.product_subscription_request
+            if not request.subscription_template.split_payment:
+                conf_email_template.send_mail(invoice.id)
