b0cd79fdd37a8b217895bfe33ceab38f765f9425
houssine <houssine.bakkali@gmail.com>
2019-10-21 19:18:32 +0200

    [IMP] remove unused import

--- product_subscription/models/subscription_request.py
+++ product_subscription/models/subscription_request.py
@@ -3,8 +3,6 @@
 #   Houssine Bakkali <houssine@coopiteasy.be>
 #   Robin Keunen <robin@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from datetime import datetime
-
 from openerp import models, fields, api, _
 from openerp.exceptions import UserError
 
