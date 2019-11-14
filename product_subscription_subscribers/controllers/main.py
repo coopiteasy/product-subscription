26a13df30f18a55e6b650f6cdde37a6a4e40af52
robin.keunen <robin@keunen.net>
2019-11-14 16:40:28 +0100

    [IMP] adapt ps_subscribers to new templates

--- product_subscription_subscribers/controllers/main.py
+++ product_subscription_subscribers/controllers/main.py
@@ -6,6 +6,46 @@ from openerp.tools.translate import _
 from openerp.addons.website_product_subscription.controllers.main import (
     WebsiteProductSubscription,
 )
+from openerp.addons.website_product_subscription.controllers.subscribe import (
+    SubscribeController,
+)
+
+
+def create_additional_subscriber(email, subscriber):
+    partner = request.env["res.partner"].sudo().search([("email", "=", email)])
+    if partner:
+        partner.parent_id = subscriber
+        return partner[0]
+    else:
+        partner = (
+            request.env["res.partner"]
+            .sudo()
+            .create(
+                {
+                    "name": email,
+                    "email": email,
+                    "customer": True,
+                    "parent_id": subscriber.id,
+                }
+            )
+        )
+        return partner
+
+
+def add_subscribers_to_subscription(subscription, params):
+    subscribers = [
+        v for k, v in params.items() if k.startswith("additionnal_email") and v
+    ]
+
+    additional_subscriber_ids = [
+        create_additional_subscriber(email, subscription.subscriber).id  # noqa
+        for email in subscribers
+    ]
+    subscription.write(
+        {"additional_subscribers": [(6, _, additional_subscriber_ids)]}
+    )
+
+    return subscription
 
 
 class WebsiteProductSubscriptionSubscribers(WebsiteProductSubscription):
@@ -32,53 +72,25 @@ class WebsiteProductSubscriptionSubscribers(WebsiteProductSubscription):
         values.update(additionnal_subscribers)
         return values
 
-    def create_additionnal_subscriber(self, email, subscriber):
-        partner = (
-            request.env["res.partner"].sudo().search([("email", "=", email)])
-        )
-        if partner:
-            partner.parent_id = subscriber
-            return partner[0]
-        else:
-            partner = (
-                request.env["res.partner"]
-                .sudo()
-                .create(
-                    {
-                        "name": email,
-                        "email": email,
-                        "customer": True,
-                        "parent_id": subscriber.id,
-                    }
-                )
-            )
-            return partner
-
     def create_subscription_request(self, **kwargs):
         subscription = super(
             WebsiteProductSubscriptionSubscribers, self
         ).create_subscription_request(**kwargs)
 
-        additionnal_subscribers = [
-            kwargs.get("additionnal_email_1", ""),
-            kwargs.get("additionnal_email_2", ""),
-            kwargs.get("additionnal_email_3", ""),
-            kwargs.get("additionnal_email_4", ""),
-            kwargs.get("additionnal_email_5", ""),
-            kwargs.get("additionnal_email_6", ""),
-        ]
-        additionnal_subscribers = filter(
-            lambda el: el, additionnal_subscribers
-        )
+        return add_subscribers_to_subscription(subscription, kwargs)
 
-        additional_subscriber_ids = [
-            self.create_additionnal_subscriber(
-                email, subscription.subscriber
-            ).id  # noqa
-            for email in additionnal_subscribers
-        ]
-        subscription.write(
-            {"additional_subscribers": [(6, _, additional_subscriber_ids)]}
-        )
 
-        return subscription
+class SubscribeControllerSubscribers(SubscribeController):
+    def process_new_subscription_basic_form(self):
+        subscription = super(
+            SubscribeControllerSubscribers, self
+        ).process_new_subscription_basic_form()
+
+        return add_subscribers_to_subscription(subscription, request.params)
+
+    def process_new_subscription_gift_form(self):
+        subscription = super(
+            SubscribeControllerSubscribers, self
+        ).process_new_subscription_gift_form()
+
+        return add_subscribers_to_subscription(subscription, request.params)
